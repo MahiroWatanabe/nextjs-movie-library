81805dbd4eb061cb2befd5301671bb9c53860e38
MahiroWatanabe <konchikuwa2@gmail.com>
2023-04-27 23:52:47 +0900

    change header styling

--- components/Header.js
+++ components/Header.js
@@ -16,17 +16,20 @@ const headerItems = [
   { title: "SEARCH", Icon: DocumentMagnifyingGlassIcon },
   { title: "ACCOUNT", Icon: UserIcon },
 ];
+
 function Header() {
   return (
-    <header className="">
-      <div>
+    <header
+      className="flex flex-col sm:flex-row m-5 justify-between 
+    items-center md:items-stretch h-auto"
+    >
+      <div className="flex flex-grow justify-evenly max-w-2xl">
         {headerItems.map((items) => (
           <HeaderItem key={items.title} title={items.title} Icon={items.Icon} />
         ))}
-
         {/* <HeaderItem title="HOME" Icon={HomeIcon} /> */}
       </div>
-      <h1>Movie-Library</h1>
+      <h1 className="text-3xl font-bold">Movie-Library</h1>
     </header>
   );
 }
